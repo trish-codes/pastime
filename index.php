db6a7d65089ff055d9fb31ba0b4fa19554ffdbca
trish <trish.codes@gmail.com>
2016-02-23 21:44:32 -0600

    Basic Index/CSS

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+    
+    <title>PASTIME</title>
+    
+    <link rel="stylesheet" href="styles/style.css" media="all" />
+    
+    
+    
+    </head>
+
+<body>
+    
+    
+    
+
+    
+</body>
+
+
+
+
+
+</html>
