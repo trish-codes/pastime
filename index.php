398bace3628bc1c2b13101d3bb3431c734d323c1
trish <trish.codes@gmail.com>
2016-02-23 23:24:00 -0600

    Signup form added

--- index.php
+++ index.php
@@ -25,7 +25,74 @@
         </div>
         
         </div>
-    
+        <div id="content">
+        
+        <div>
+            <img src="images/">
+        </div>
+        <div><form action="" method="post">
+            <table>
+                <tr>
+                    <td alight="right">Name:</td>
+                    <td>
+                    <input type="text" name="u_name" placeholder="Enter your name" />
+                    </td>
+                </tr>
+                
+                <tr>
+                    <td alight="right">Password:</td>
+                    <td>
+                    <input type="text" name="u_pass" placeholder="Enter your password" />
+                    </td>
+                </tr>
+                
+                <tr>
+                    <td alight="right">Email:</td>
+                    <td>
+                    <input type="text" name="u_email" placeholder="Enter your email" />
+                    </td>
+                </tr>
+                
+                <tr>
+                    <td alight="right">Country:</td>
+                    <td>
+                    <select name="u_country">
+                    <option>Select a country</option>
+                    <option>United States</option> 
+                    <option>Canada</option>
+                    <option>United Kingdom</option>
+                    <option>France</option>
+                    </select>
+                    </td>
+                </tr>
+                
+                  <tr>
+                    <td alight="right">Gender:</td>
+                    <td>
+                    <select name="u_country">
+                    <option>Select a gender</option>
+                    <option>Male</option> 
+                    <option>Female</option>
+                    </select>
+                    </td>
+                </tr>
+                
+                <tr>
+                    <td alight="right">Born:</td>
+                    <td>
+                    <input type="date" name="u_birthday" />
+                    </td>
+                </tr>
+                
+                 <tr>
+                    <td>
+                    <button name="sign_up">Sign Up</button>
+                    </td>
+                </tr>
+            </table>
+            </form>
+            </div>
+        </div>
     </div>
     <!-----------------container ends------------------------------------>
 
