ac97b054a2160d1f75f9d7a8c49283e807b3f260
trish <trish.codes@gmail.com>
2016-02-23 23:56:07 -0600

    Fixed and styled Sign Up Form

--- index.php
+++ index.php
@@ -6,9 +6,8 @@
     <title>PASTIME</title>
     
     <link rel="stylesheet" href="styles/style.css" media="all" />
-    
-    
-    
+    <link href='https://fonts.googleapis.com/css?family=Oswald' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Open+Sans+Condensed:300' rel='stylesheet' type='text/css'>
     </head>
 
 <body>
@@ -19,46 +18,53 @@
         <div id="header">
             <img src="images/logo.png">
         <form method="post" action="" id="form1">
-            <input type="text" name="email" placeholder="Email"/>
-            <input type="password" name="pass" placeholder="*****"/>            <button name="login">Login</button>
+            <strong>Email:</strong><input type="text" name="email" placeholder="Email" required="required"/>
+            <strong>Password:</strong>
+            <input type="password" name="pass" placeholder="*****" required="required"/>            
+        <button name="login">Login</button>
         </form>
         </div>
         
         </div>
+        
+        
+     <!----------Content form area starts ------------------------------>   
         <div id="content">
         
         <div>
             <img src="images/">
         </div>
-        <div><form action="" method="post">
+        <div id="form2">
+            <form action="" method="post">
+            <h2>Sign Up Here</h2>
             <table>
                 <tr>
-                    <td alight="right">Name:</td>
+                    <td align="right">Name:</td>
                     <td>
-                    <input type="text" name="u_name" placeholder="Enter your name" />
+                    <input type="text" name="u_name" placeholder="Enter your name" required="required"/>
                     </td>
                 </tr>
                 
                 <tr>
-                    <td alight="right">Password:</td>
+                    <td align="right">Password:</td>
                     <td>
-                    <input type="text" name="u_pass" placeholder="Enter your password" />
+                    <input type="text" name="u_pass" placeholder="Enter your password" required="required"/>
                     </td>
                 </tr>
                 
                 <tr>
-                    <td alight="right">Email:</td>
+                    <td align="right">Email:</td>
                     <td>
-                    <input type="text" name="u_email" placeholder="Enter your email" />
+                    <input type="text" name="u_email" placeholder="Enter your email" required="required" />
                     </td>
                 </tr>
                 
                 <tr>
-                    <td alight="right">Country:</td>
+                    <td align="right">Country:</td>
                     <td>
                     <select name="u_country">
                     <option>Select a country</option>
-                    <option>United States</option> 
+                    <option>United States</option`> 
                     <option>Canada</option>
                     <option>United Kingdom</option>
                     <option>France</option>
@@ -67,7 +73,7 @@
                 </tr>
                 
                   <tr>
-                    <td alight="right">Gender:</td>
+                    <td align="right" required="required">Gender:</td>
                     <td>
                     <select name="u_country">
                     <option>Select a gender</option>
@@ -78,7 +84,7 @@
                 </tr>
                 
                 <tr>
-                    <td alight="right">Born:</td>
+                    <td align="right">Born:</td>
                     <td>
                     <input type="date" name="u_birthday" />
                     </td>
