cc68d03efdd223048ded8c77c7dd349976454ea4
trish <trish.codes@gmail.com>
2016-02-24 22:31:06 -0600

    error fixed

--- functions/functions.php
+++ functions/functions.php
@@ -1,6 +1,6 @@
 <?php
 
-$con = mysqli_connect("localhost","root","","pastime") or die ("Connection was not established");
+$con = mysqli_connect("localhost" ,"root" ,"" ,"pastime");
 
 function InsertUser(){
     
